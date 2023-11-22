4cfd20410904f5ef7837036d28db9aad3ef0237e
saawaahh <112594593+saawaahh@users.noreply.github.com>
2023-11-22 04:51:59 -0600

    the one that really got away
    so im not sure how maybe its cause im tired and dslyxic but this one actually revert back to a previous version even after ive edited it multiple times today. this does fix the abaility to view a single entry. so now the issue is viewing details after submitting an entry

--- src/pages/entries/[pid].tsx
+++ src/pages/entries/[pid].tsx
@@ -4,17 +4,14 @@ import Head from "next/head";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 import moment from "moment";
-
-
+import { TrashIcon } from "@heroicons/react/24/solid";
 
 const Entry = () => {
-  
   const { status: sessionStatus } = useSession();
   const { replace, query } = useRouter();
   const entryId = Array.isArray(query.pid) ? query.pid[0] : query.pid;
 
-
-  const { data: entryData, status: entryStatus } =
+  const { data: entryData, refetch: refetchEntry } =
     api.journalling.getEntryById.useQuery(
       { id: entryId! },
       {
@@ -28,57 +25,41 @@ const Entry = () => {
     },
   });
 
-
   useEffect(() => {
     if (sessionStatus === "unauthenticated") {
       replace("/");
     }
   }, [sessionStatus]);
+
   return (
     <>
       <Head>
         <title>Entry</title>
       </Head>
-
-      <section className="mt-32 flex flex-col justify-center gap-10"> 
-      {
-
-        entryData !== null && (
-          <div className="mx-auto flex flex-col gap-5">
+      <section className="mt-32 flex flex-col justify-center gap-10">
+        {entryData !== null && (
+          <div className="mx-auto flex w-1/2 flex-col gap-5">
             <div className="flex flex-row items-center justify-between">
-              <h1 className="text-3xl font-sans font-extrabold text-gray-50 ">
-                {moment(entryData?.dateCreated).format("MMM Do YYYY")}
-                </h1>
-
-            </div>
-              <p className="font-montserrat whitespace-pre-line bg-gray-900 p-5 lext-lg">
-                {entryData?.content}
-              </p>
-
-          </div>
-
-        )
-
-      }
+              <h1 className="font-poppins text-3xl font-extrabold text-gray-50">
+                {moment(entryData?.dateCreated).format("MMMM Do YYYY")}
+              </h1>
         
-      
-      
-      
-      
-      
+              <button
+                className="rounded-sm bg-gradient-to-br from-gray-700 to-gray-800 p-2"
+                onClick={() => deletionMutation({ id: entryId! })}
+              >
+                <TrashIcon width={25} className="text-gray-50" />
+              </button>
+            </div>
+
+            <p className="font-montserrat whitespace-pre-line bg-gray-900 p-5 text-lg text-gray-50">
+              {entryData?.content}
+            </p>
+          </div>
+        )}
       </section>
-    
-    
-    
-    
     </>
-
-
-
-
-
-  )
+  );
 };
-
 
 export default Entry;
