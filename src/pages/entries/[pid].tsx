febbb3c8340c70c889a798fb8dc81137c7eacb14
saawaahh <112594593+saawaahh@users.noreply.github.com>
2023-11-25 03:05:26 -0600

    idkkkk

--- src/pages/entries/[pid].tsx
+++ src/pages/entries/[pid].tsx
@@ -1,4 +1,4 @@
-\import { api } from "@/utils/api";
+import { api } from "@/utils/api";
 import { useSession } from "next-auth/react";
 import Head from "next/head";
 import { useRouter } from "next/router";
