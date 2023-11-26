7370ecc07c5a840ffd635a83fbaa2f2d18551ec3
vjordan-cs <vjordan.cs@gmail.com>
2023-11-26 13:58:56 -0600

    update landing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,20 +9,53 @@ export default function Home() {
 
   return (
     <>
+      <div className="h-screen md:flex">
+        <div className="flex h-2/3 items-center justify-center bg-slate-500 md:h-screen md:w-2/3">
+          <div className="text-center">
+            <div className="text-5xl md:text-6xl">☘️ BrainSprout</div>
+            <div className="p-10">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
+              exercitationem dolor consequuntur? Atque corrupti, illo magni
+              aliquid maxime animi omnis vero illum soluta dolore officiis
+              doloremque excepturi quae culpa velit!
+            </div>
+          </div>
+        </div>
+        <div className="flex h-1/3 items-center justify-center bg-red-200 md:h-screen md:w-1/3">
+          <div className="text-center">
+            <div className="mb-10 text-xl">Sign In</div>
+            <button
+              onClick={sessionData ? () => void signOut() : () => void signIn()}
+              className="rounded-lg border-2 border-slate-100 bg-slate-100 p-6"
+            >
+              {sessionData ? "Sign Out" : "Sign In"}
+            </button>
+          </div>
+        </div>
+      </div>
       <Head>
         <title>BrainSprout</title>
-        <meta name="description" content="A journalling app to promote recovery" />
+        <meta
+          name="description"
+          content="A journalling app to promote recovery"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div
-        className={`flex h-screen items-center bg-${sessionData ? 'bg-cyan-900' : 'cover bg-center'}`}
-        style={sessionData ? { backgroundColor: 'bg-cyan-900' } : { backgroundImage: `url(/landing.jpeg)` }}
+        className={`flex h-screen items-center bg-${
+          sessionData ? "bg-cyan-900" : "cover bg-center"
+        }`}
+        style={
+          sessionData
+            ? { backgroundColor: "bg-cyan-900" }
+            : { backgroundImage: `url(/landing.jpeg)` }
+        }
       >
         <div className="m-auto mt-64 flex flex-col justify-center gap-5 text-center align-middle">
           <h1 className="font-poppins bg-gradient-to-br from-gray-900 to-gray-900 box-decoration-slice bg-clip-text p-2 text-7xl font-extrabold text-transparent">
             BrainSprout
           </h1>
-          <h2 className="font-montserrat text-xl text-bg-lime-900">
+          <h2 className="font-montserrat text-bg-lime-900 text-xl">
             sprout your recovery
           </h2>
           <button
