5af0029594bd16a7f18603014beb97636ae41a25
saawaahh <112594593+saawaahh@users.noreply.github.com>
2024-03-22 22:31:53 -0500

    Delete src directory

--- src/components/Loading.tsx
+++
@@ -1,9 +0,0 @@
-const Loading = () => (
-    <div className="flex h-screen justify-center">
-      <h1 className="font-poppins m-auto text-4xl font-bold text-gray-50">
-        Loading...
-      </h1>
-    </div>
-  );
-  
-  export default Loading;
